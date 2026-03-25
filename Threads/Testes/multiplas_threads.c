76a88a7906e7b2104d4fefe575cc22591c907233
JpEscobarM <jp.escobarmartins@gmail.com>
2026-03-25 08:40:27 -0300

    add

--- Threads/Testes/multiplas_threads.c
+++ Threads/Testes/multiplas_threads.c
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include <time.h>
 
-#define NUM_THREADS 10
+#define NUM_THREADS 20
+
+int ERRO_TH = 0;
+int SUCESSO_TH = 1;
 
 
 
@@ -13,10 +16,29 @@ typedef struct{
     int thread_id;
     int num;
     int sec;
+    int *retorno;
 
 }ThreadArgs;
 
-void* generate_random(){
+void* generate_random(void *args)
+{
+
+    ThreadArgs *args_t = (ThreadArgs *) args;
+
+
+    printf("\n\nTHREAD_ID{%2d}|NUMERO{%2d}", args_t->thread_id,args_t->num);
+
+    if (args_t->num % 2 == 0)
+    {
+        printf("\nÉ par, vai dormir por %d",args_t->sec);
+        sleep(args_t->sec);
+
+        pthread_exit((void *) &SUCESSO_TH);
+    }
+    else
+    {
+        pthread_exit((void *) &ERRO_TH);
+    }
 
 }
 
@@ -33,12 +55,24 @@ int main()
     for(int i =0 ; i< NUM_THREADS ; i ++){
         tArgs[i].thread_id =i;
         tArgs[i].sec=i;
-        tArgs[i].num = rand()%1000;
+        tArgs[i].num = rand();
     }
 
     for(int i =0 ; i< NUM_THREADS ; i ++){
+        pthread_create(&threads_v[i],NULL,generate_random,(void*) &tArgs[i]);
+    }
+
+    for(int i =0 ; i< NUM_THREADS ; i ++)
+    {
+        pthread_join(threads_v[i],(void **) &tArgs[i].retorno);
+
+
+        printf("\nTHREAD RETORNO %d",*tArgs[i].retorno);
+
 
     }
+
+
 
 
     return 0;
