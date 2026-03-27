76acb8eb16d27eba9589711d6c805841e4ca8c36
JpEscobarM <jp.escobarmartins@gmail.com>
2026-03-27 10:37:07 -0300

    teste cmake

---
+++ Threads/teste_cmake/build/CMakeFiles/teste_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teste_cmake.dir/main.c.o"
+  "teste_cmake"
+  "teste_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/teste_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
